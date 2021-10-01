778c36b60ff04faa34d5d746911be272dc8fca03
El Naz <nurulnazliahmurtadha@gmail.com>
2021-10-01 21:14:38 +0800

    change kedua

--- event handler/js/event.js
+++ event handler/js/event.js
@@ -6,3 +6,7 @@ let elNamaPengguna = document.getElementById("namapengguna")
 elNamaPengguna.addEventListener("keyup",function(){
     elNamaPengguna.value = transformUppercase(elNamaPengguna.value)
 })
+
+function CheckPassword(){
+    let elKataLaluan =document.getElementById("katalaluan")
+}
